b6a0ec9dcc60128ca533aca59d2ca047715bb2c9
AqeelAhmedBaloch <ahmedbalochaqeel@gmail.com>
2025-01-14 10:30:28 +0500

    First Commit

---
+++ next-sanity-blog/app/(blog)/alert-banner.tsx
@@ -0,0 +1,8 @@
+export default function AlertBanner() {
+
+  return (
+    <div className="fixed top-0 left-0 z-50 w-full border-b bg-white/95 text-black backdrop-blur">
+      <div className="py-2 text-center text-sm">BLOGS</div>
+    </div>
+  );
+}
